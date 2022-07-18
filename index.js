4be463beac411a81c1e1119a7755ed748145a38b
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-07-18 21:38:44 +0200

    feat:  Add ping-pong api

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//TODO: localhost:3001/api/ping -> "pong"
+//TODO: localhost:3001/api/ping -> "pong"  ###DONE###
+
+const express = require('express');
+const app = express();
+
+const PORT = 3001;
+
+app.get('/api/ping', (req, res) =>{
+    res.send('pong');
+});
+
+app.listen(PORT, () =>{'Listening on port 3001'});
+
