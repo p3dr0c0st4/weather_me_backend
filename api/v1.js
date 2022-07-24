c3cbe7c67a4322f2478fd4c4d33a1d4750129879
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-07-24 15:33:07 +0200

    chore: Importing Schemas to routes

--- api/v1.js
+++ api/v1.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+//Schemas import
+const mongodb = require('../mongodb');
+const humiditySchema = require('../schemas/humidity-history-schema');
+const temperatureSchema = require('../schemas/temperature-history-schema');
+const sensorsSchema = require('../schemas/sensors-schema');
+const locationSchema = require('../schemas/location-schema');
 
 app.get("/api/v1/sensors/:id/history", (req, res) =>{
     req.get();
