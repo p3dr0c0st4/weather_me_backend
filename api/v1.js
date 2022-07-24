7fed7eb0578d4e5905d1195cb4d7f7a91cc029b9
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-07-24 20:08:26 +0200

    chore:  Try to connect to database(test's)

--- api/v1.js
+++ api/v1.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const app = express();
+const router = express.Router();
+
 //Schemas import
 const mongodb = require('../mongodb');
 const humiditySchema = require('../schemas/humidity-history-schema');
@@ -7,14 +8,30 @@ const temperatureSchema = require('../schemas/temperature-history-schema');
 const sensorsSchema = require('../schemas/sensors-schema');
 const locationSchema = require('../schemas/location-schema');
 
-app.get("/api/v1/sensors/:id/history", (req, res) =>{
-    req.get();
+
+router.get("api/v1/sensors/:id/history", async (req, res) =>{
+    try {
+        const temperatureHistory = await temperatureSchema.find(location)
+        const humidityHistory = await humiditySchema.find()
+        res.json(temperatureHistory)
+        res.json(humidityHistory)
+    } catch{
+        res.status(500).json({message: err.message})
+    }
 });
 
-app.post("/api/v1/sensors/:id/history/temperature", (req, res) =>{
-    res.send();
+router.post("api/v1/sensors/:id/history/temperature", async (req, res) =>{
+    try {
+        const temperatureHistory = await temperatureSchema.find()
+    } catch{
+        res.status(500).json({message: err.message})
+    }
 });
 
-app.post("/api/v1/sensors/:id/history/humidity", (req, res) =>{
-    res.send();
+router.post("api/v1/sensors/:id/history/humidity",async (req, res) =>{
+    req.send({
+
+    });
 })
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 //TODO: localhost:3001/api/ping -> "pong"  ###DONE###
-
+import { db } from './mongodb';
 const express = require('express');
 const app = express();
 
 const PORT = 3001;
 
-app.get('/api/ping', (req, res) =>{
-    res.send('pong');
-});
+
+db.on('error', (error) => console.log(error));
+
+app.use(express.json());
+const routes = require('./api/v1');
+
+app.use('/', routes);
 
 app.listen(PORT, () =>{'Listening on port 3001'});
 
