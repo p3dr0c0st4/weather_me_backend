246601992beb30b3ec82dffb5c60ec2b4871fb79
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-07-25 21:03:51 +0200

    fix:  Connections fixing

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,21 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://127.0.0.1/wmdata';
+const express = require('express');
 
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
 
-export const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Connected to Database'));
+
+ 
+
+app.use(express.json());
+const routes = require('./api/v1');
+
+app.use('/api/v1', routes);
+ 
+
+const PORT = 3001;
+app.listen(PORT, () =>{'Listening on port 3001'});
