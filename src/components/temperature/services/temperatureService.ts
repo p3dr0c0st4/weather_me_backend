e5f11397a1e6318c1d856bd0bb03a2856f8bab43
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-08-29 20:27:15 +0200

    refactor: api testing

--- src/components/temperature/services/temperatureService.ts
+++ src/components/temperature/services/temperatureService.ts
@@ -1,4 +1,7 @@
 import temperatureSchema from '@db/implementation/mongodb/schemas/temperatureSchema';
+import { create } from 'domain';
+
+
 
 export type saveTemperatureParams = {
   location: string;
@@ -7,13 +10,21 @@ export type saveTemperatureParams = {
 };
 
 const saveTemperature = (data: saveTemperatureParams) => {
+
   const temp = new temperatureSchema({
     id: undefined,
     location: data.location,
     date: data.date,
     temperature: data.temperature,
   });
-  return temp.save();
+
+  return temp.updateOne({
+    id: 15,
+    location: 'apartamento',
+    date: 808080,
+    temperature: 25
+  })
+
 };
 const readTemperature = () => {};
 export { saveTemperature, readTemperature };
