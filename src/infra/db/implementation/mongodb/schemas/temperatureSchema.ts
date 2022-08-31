4920e893b9b49b0f0ebaf4d8fa5d943570b45ad4
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-08-31 12:19:06 +0200

    feat: add interfaceSchema to temperaturteSchema
    Co-authored-by: PɤCo <p3dr0c0st4@users.noreply.github.com>

--- src/infra/db/implementation/mongodb/schemas/temperatureSchema.ts
+++ src/infra/db/implementation/mongodb/schemas/temperatureSchema.ts
@@ -1,10 +1,27 @@
-import { Schema, model } from 'mongoose';
+import { ITemperatureDto } from '@temperature/controller/temperatureController';
+import { Schema, model, Document } from 'mongoose';
+
+interface ITemperatureSchema extends Document{
+  id:string,
+  temperature: number,
+  location: string,
+  date: number
+}
+
+export const toDto = (obj:ITemperatureSchema):ITemperatureDto => {
+  return {
+    id: obj._id,
+    temperature: obj.temperature,
+    date: obj.date,
+    location: obj.location
+  }
+} 
 
 const temperatureSchema = new Schema({
-  id: Number,
-  location: String,
-  date: Number,
-  temperature: Number,
+  id: {type:Number, require:true},
+  location: {type: String},
+  date: {type:Number},
+  temperature: {type:Number},
 });
 
-export default model('temperature', temperatureSchema);
+export default model<ITemperatureSchema>('temperature', temperatureSchema);
