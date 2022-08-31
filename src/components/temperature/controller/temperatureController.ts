bd076ff3546c9d9ca7cbae29cacf085adeaf095e
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-08-31 12:23:16 +0200

    feat: create add and read function connected to db

--- src/components/temperature/controller/temperatureController.ts
+++ src/components/temperature/controller/temperatureController.ts
@@ -1,16 +1,25 @@
 import {NextFunction, Response,Request} from 'express'
 import {
   saveTemperature,
+  readTemperature,
   saveTemperatureParams,
 } from '@temperature/services/temperatureService';
 
-  const getTemperature = (req: Request, res: Response, next: NextFunction)=>{
-    const temp: saveTemperatureParams = {
-      location: 'casa',
-      date: 12345,
-      temperature: 3012,
-    };
-    res.json(temp);
+export interface ITemperatureDto{
+  id:string,
+  temperature: number,
+  location: string,
+  date: number
+}
+
+
+  const getTemperature = async (req: Request, res: Response, next: NextFunction)=>{
+
+    //Get all docs from DB
+    const tempArr:ITemperatureDto[]  = await readTemperature();
+    //check if failed and return 500
+    //if success return Array of docs
+    res.status(200).json(tempArr);
   }
   const postTemperature = async (req: Request, res: Response, next: NextFunction)=>{
     const temp: saveTemperatureParams = {
@@ -19,8 +28,16 @@ import {
       temperature: req.body.temperature,
     };
 
-    const resp = await saveTemperature(temp);
-    return res.json(resp);
+    const result = await saveTemperature(temp);
+    if(!result){
+      return res.status(500).json({
+        success:false,
+        message: 'Failed to save temperature'
+      })
+    }
+    return res.status(201).json({
+        success:true,
+      });
   }
 
   export {
