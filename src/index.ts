a6ddec84efe5f120f06a932a1cfde3073d0c33a6
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-08-31 12:19:31 +0200

    refactor: add cors

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ dotenv.config({ path: __dirname+'/.env' });
 import express from 'express';
 import routesV1 from './api/v1';
 const app = express();
+import cors from 'cors';
 import {init} from './infra/db/implementation/mongodb/mongodb';
 
 const PORT = 3000;
@@ -10,6 +11,7 @@ const PORT = 3000;
 const initDB = () => {
   init();
 };
+app.use(cors());
 app.use(express.json());
 app.use('/api/v1', routesV1);
 app.get('/ping', (req, res) => {
