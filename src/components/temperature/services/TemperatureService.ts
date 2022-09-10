8155c6c3923e25507fb66e236df07236883aa492
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-09-10 20:09:53 +0200

    feat: add update to CRUD (WIP)

--- src/components/temperature/services/TemperatureService.ts
+++ src/components/temperature/services/TemperatureService.ts
@@ -3,15 +3,18 @@ import { TemperatureDto } from '@temperature/dtos/temperatureDto';
 
 class TemperatureService{
   constructor(private db:IDatabase){
-
   } 
 
   saveTemperature(data: TemperatureDto) {
-    return this.db.create(data);
+    return this.db.temperature.create(data);
   };
 
   async readTemperature (filter?:any):Promise<TemperatureDto[]>  {
-    return this.db.read(filter);
+    return this.db.temperature.read(filter);
+  };
+
+  async updateTemperature (filter?:any):Promise<TemperatureDto[]>  {
+    return this.db.temperature.update(filter);
   };
 }
 export default TemperatureService;
--- src/infra/db/interfaces/ICrud.ts
+++ src/infra/db/interfaces/ICrud.ts
@@ -3,6 +3,6 @@ import { TemperatureDto } from "@temperature/dtos/temperatureDto";
 export default interface ICrud {
   create(data: TemperatureDto):Promise<boolean>;
   read(filter:any):Promise<TemperatureDto[]>;
-  update():void;
+  update(filter:any):Promise<TemperatureDto[]>;
   delete():void;
 }
