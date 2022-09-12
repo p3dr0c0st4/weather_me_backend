9651ae688e0cfe78cb854733cf16c86a8e9e0b97
Pedro Costa <peco@localhost.pt>
2022-09-12 21:22:04 +0100

    updateById method

--- src/infra/db/implementation/mongodb/repository/TemperatureRepository.ts
+++ src/infra/db/implementation/mongodb/repository/TemperatureRepository.ts
@@ -2,9 +2,9 @@ import temperatureSchema, {toDto} from '@db/implementation/mongodb/schemas/tempe
 import ICrud from '@db/interfaces/ICrud';
 import { TemperatureDto } from '@temperature/dtos/temperatureDto';
 
-class TemperatureRepository implements ICrud{
-    create(data: TemperatureDto): Promise<boolean> {
-       const temp = new temperatureSchema({
+class TemperatureRepository implements ICrud {
+  create(data: TemperatureDto): Promise<boolean> {
+    const temp = new temperatureSchema({
       id: undefined,
       location: data.location,
       date: data.date,
@@ -12,46 +12,36 @@ class TemperatureRepository implements ICrud{
     });
 
     return Promise.resolve(temp.save() !== null);
+  }
+
+  async read(filter: any): Promise<TemperatureDto[]> {
+    const docs = await temperatureSchema.find({ filter });
+    return docs.map((x) => toDto(x));
+  }
+
+  async updateById(
+    id: string,
+    data: TemperatureDto
+  ): Promise<TemperatureDto | never> {
+    let doc = await temperatureSchema
+      .findOneAndUpdate(
+        { _id: id },
+        { data },
+        {
+          new: true,
+        }
+      )
+      .exec();
+
+    if (!doc) {
+      throw new Error('Could not update temperature');
     }
 
+    return toDto(doc);
+  }
 
-    async read(filter: any): Promise<TemperatureDto[]> {
-            const docs = await temperatureSchema.find({filter});
-    return docs.map(x => toDto(x));
-    }
-
-
-    async update(filter: any): Promise<TemperatureDto[]> {
-
-        let doc = await temperatureSchema.findOneAndUpdate(filter, update, {
-            new: true
-          });
-
-          doc.map(x => {
-            toDto(x);
-            return {
-                location: x.location,
-                date: x.date,
-                temperature: x.temperature
-            }
-          }); 
-
-       /*  const docs = await temperatureSchema.find({filter})
-
-        temperatureSchema.updateOne(filter, (docs:TemperatureDto)=>{
-            
-            return Promise.resolve (docs = {
-                location: docs.location,
-                date: docs.date,
-                temperature: docs.temperature
-            })
-        })
-        return docs; */
-        
-    }
-
-    delete(): void {
-        throw new Error('Method not implemented.');
-    }
+  deleteById(): void {
+    throw new Error('Method not implemented.');
+  }
 }
 export default TemperatureRepository;
--- src/infra/db/interfaces/ICrud.ts
+++ src/infra/db/interfaces/ICrud.ts
@@ -1,8 +1,8 @@
 import { TemperatureDto } from "@temperature/dtos/temperatureDto";
 
 export default interface ICrud {
-  create(data: TemperatureDto):Promise<boolean>;
-  read(filter:any):Promise<TemperatureDto[]>;
-  update(filter:any):Promise<TemperatureDto[]>;
-  delete():void;
+  create(data: TemperatureDto): Promise<boolean>;
+  read(filter: any): Promise<TemperatureDto[]>;
+  updateById(id: string, data: TemperatureDto): Promise<TemperatureDto | never>;
+  deleteById(id: string): Promise<boolean>;
 }
