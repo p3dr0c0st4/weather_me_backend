a55c112c8ba3a1391a61ccb02fa3e0f6819899db
Pedro Costa <peco@localhost.pt>
2022-09-12 21:22:04 +0100

    updateById method

--- src/components/temperature/controller/TemperatureController.ts
+++ src/components/temperature/controller/TemperatureController.ts
@@ -1,21 +1,21 @@
 import { TemperatureDto } from '@temperature/dtos/temperatureDto';
 import TemperatureService from '../services/TemperatureService';
 
-import {NextFunction, Response,Request} from 'express';
+import { NextFunction, Response, Request } from 'express';
 
-class TemperatureController{
-  constructor(private readonly temperatureService:TemperatureService){
-  }
+class TemperatureController {
+  constructor(private readonly temperatureService: TemperatureService) {}
 
-  async getTemperature(req: Request, res: Response, next: NextFunction){
+  async getTemperature(req: Request, res: Response, next: NextFunction) {
     //Get all docs from DB
-    const tempArr:TemperatureDto[]  = await this.temperatureService.readTemperature();
+    const tempArr: TemperatureDto[] =
+      await this.temperatureService.readTemperature();
     // //check if failed and return 500
     // //if success return Array of docs
     res.status(200).json(tempArr);
-  } 
+  }
 
-  async postTemperature(req: Request, res: Response, next: NextFunction){
+  async postTemperature(req: Request, res: Response, next: NextFunction) {
     const temp: TemperatureDto = {
       date: req.body.date,
       location: req.body.location,
@@ -23,37 +23,50 @@ class TemperatureController{
     };
 
     const result = await this.temperatureService.saveTemperature(temp);
-    if(!result){
+    if (!result) {
       return res.status(500).json({
-        success:false,
-        message: 'Failed to save temperature'
-      })
-    }
-    return res.status(201).json({
-        success:true,
+        success: false,
+        message: 'Failed to save temperature',
       });
-  }
-
-   async updateTemperature(req: Request, res: Response, next:NextFunction){
-
-    // Get docs from db
-    let temp:TemperatureDto[]  = await this.temperatureService.updateTemperature();
-    //check if failed and return 500
-    if(!temp){
-      return res.status(500).json({
-        sucess: false,
-        message: 'Failed to update Temperature Sensor'
-      })
     }
-
-    //Send response
     return res.status(201).json({
-      success:true,
-      message: 'Successfully updated on Database'
+      success: true,
     });
   }
 
+  async updateTemperature(req: Request, res: Response, next: NextFunction) {
+    // Get docs from db
+    try {
+      const temp: TemperatureDto = {
+        date: req.body.date,
+        location: req.body.location,
+        temperature: req.body.temperature,
+      };
+      const id = req.params.id;
 
+      let result: TemperatureDto =
+        await this.temperatureService.updateTemperature(id, temp);
+      //check if failed and return 500
+      if (!result) {
+        return res.status(500).json({
+          sucess: false,
+          message: 'Failed to update Temperature Sensor',
+        });
+      }
+      //Send response
+      return res.status(200).json({
+        success: true,
+        message: 'Successfully updated on Database',
+        data: result,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        sucess: false,
+        message: error,
+      });
+    }
+  }
 }
 
 export default TemperatureController;
--- src/components/temperature/services/TemperatureService.ts
+++ src/components/temperature/services/TemperatureService.ts
@@ -1,20 +1,22 @@
 import { IDatabase } from '@db/interfaces/IDatabase';
 import { TemperatureDto } from '@temperature/dtos/temperatureDto';
 
-class TemperatureService{
-  constructor(private db:IDatabase){
-  } 
+class TemperatureService {
+  constructor(private db: IDatabase) {}
 
   saveTemperature(data: TemperatureDto) {
     return this.db.temperature.create(data);
-  };
+  }
 
-  async readTemperature (filter?:any):Promise<TemperatureDto[]>  {
+  async readTemperature(filter?: any): Promise<TemperatureDto[]> {
     return await this.db.temperature.read(filter);
-  };
+  }
 
-  async updateTemperature (filter?:any, update?:any):Promise<TemperatureDto[]>  {
-    return await this.db.temperature.update(filter);
-  };
+  async updateTemperature(
+    id: string,
+    update: TemperatureDto
+  ): Promise<TemperatureDto | never> {
+    return await this.db.temperature.updateById(id, update);
+  }
 }
 export default TemperatureService;
