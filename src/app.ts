9189a2b5ae49c69cd9262e7493a0b2bc6d4932ef
Pedro Costa <peco@localhost.pt>
2022-10-10 21:24:57 +0100

    add spy and express to test

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,19 @@
 import * as dotenv from "dotenv";
-dotenv.config({ path: __dirname+'/.env' }); 
 import express from 'express';
 import routesV1 from './api/v1';
 import cors from 'cors';
 import { IDatabase } from '@db/interfaces/IDatabase';
 
-
-
 const application = async (db:IDatabase) => {
   //Initialize db
   db.init();
-
 
   const app = express();
   app.use(cors());
   app.use(express.json());
 
 
-  app.use('/api/v1', await routesV1(db));
+  app.use('/api/v1', routesV1(db));
   app.get('/ping', (req, res) => {
     res.json({ success: true });
   });
--- test/components/temperature/temperature.test.ts
+++ test/components/temperature/temperature.test.ts
@@ -1,45 +1,40 @@
 import supertest from 'supertest';
-import server from '../../../src/infra/server/server'
-import mongoose, { mongo } from 'mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server';
+import express from 'express'
+import TemperatureController from '../../../src/components/temperature/controller/TemperatureController'
+import TemperatureService from '../../../src/components/temperature/services/TemperatureService';
 import MockDb from '../../../src/infra/db/implementation/mockDb/MockDb';
-import application from '../../../src/app';
+
+const mockDb = new MockDb();
+const temperatureService = new TemperatureService(mockDb)
+const temperatureCtrl = new TemperatureController(temperatureService);
 
 
-const mockDb = new MockDb(); 
-const app = application(mockDb);
+const app = express()
+  app.use('/api/v1/temperature', async (req,res,next)=>{
+    return await temperatureCtrl.getTemperature(req,res,next)
+  });
 
 describe('temperature', () =>{
-
-    beforeAll(async () => {
-        const mongoServer = await MongoMemoryServer.create();
-
-        await mongoose.connect(mongoServer.getUri());
-    })
-
-    afterAll (async() =>{
-        await mongoose.disconnect();
-        await mongoose.connection.close();
-    })
-
-
     describe('get temperature routes', () =>{
-        describe('If temperature does not exist', () =>{         
+        describe('If temperature does not exist', () =>{ 
             it('return a 200', async () =>{
-                
+                const tempServiceSpy = jest
+                  .spyOn(temperatureService, 'readTemperature')
+                  .mockImplementation(() => Promise.resolve([]));
                 const res = await supertest(app).get(`/api/v1/temperature`);
+
                 expect(res.statusCode).toBe(200);
+                expect(res.body).toEqual([]);
             })
         });
-
     });
-    describe('post temperature routes', () =>{
+    // describe('post temperature routes', () =>{
 
-    });
-    describe('patch temperature routes', () =>{
+    // });
+    // describe('patch temperature routes', () =>{
 
-    });
-    describe('delete temperature routes', () =>{
+    // });
+    // describe('delete temperature routes', () =>{
 
-    });
+    // });
 });
