4b357671336965a8f8ff12606bc9bd7ec93a3202
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-10-19 17:16:52 +0200

    refactor:  ICrud -> ICrudTemperature

--- src/infra/db/implementation/mockDb/mockDb.ts
+++ src/infra/db/implementation/mockDb/mockDb.ts
@@ -1,9 +1,9 @@
-import ICrud from '@db/interfaces/ICrud';
+import ICrudTemperature from '@db/interfaces/ICrudTemperature';
 import { IDatabase } from '@db/interfaces/IDatabase';
 import TemperatureRepository from './repositories/TemperatureRepository';
 
 export default class MockDb implements IDatabase {
-  temperature: ICrud;
+  temperature: ICrudTemperature;
 
   constructor() {
     this.temperature = new TemperatureRepository();
