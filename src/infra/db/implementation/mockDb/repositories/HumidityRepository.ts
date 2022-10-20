27d88124bec42cb56e2a674b59a0e5bb8eb7611d
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-10-20 06:51:48 +0200

    feat: add humidity repositories to db and Mock db

---
+++ src/infra/db/implementation/mockDb/repositories/HumidityRepository.ts
@@ -0,0 +1,36 @@
+import ICrudHumidity from '@db/interfaces/ICrudHumidity';
+import { HumidityDto } from '@humidity/dtos/humidityDto';
+import { randomBytes } from 'crypto';
+
+export default class HumidityRepository implements ICrudHumidity {
+  private data: Map<string, HumidityDto>;
+
+  constructor() {
+    this.data = new Map<string, HumidityDto>();
+  }
+
+  create(data: HumidityDto): Promise<boolean> {
+    const id = randomBytes(16).toString('hex');
+    this.data.set(id, data);
+    return Promise.resolve(true);
+  }
+  read(filter: any): Promise<HumidityDto[]> {
+    return Promise.resolve(
+      [...this.data.entries()].map((x) => {
+        return {
+          id: x[0],
+          humidity: x[1].humidity,
+          location: x[1].location,
+          date: x[1].date,
+        };
+      })
+    );
+  }
+  updateById(id: string, data: HumidityDto): Promise<HumidityDto> {
+    throw new Error('Method not implemented.');
+  }
+  deleteById(id: string): Promise<boolean> {
+    this.data.delete(id);
+    return Promise.resolve(true);
+  }
+}
