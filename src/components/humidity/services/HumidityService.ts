b41a58bb88ad08035f1ae746ad8ea986edcb298c
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-10-21 07:06:14 +0200

    feat: add humidity service

---
+++ src/components/humidity/services/HumidityService.ts
@@ -0,0 +1,26 @@
+import { IDatabase } from '@db/interfaces/IDatabase';
+import { HumidityDto } from '@humidity/dtos/humidityDto';
+
+export default class HumidityService {
+  constructor(private db: IDatabase) {}
+
+  saveHumidity = (data: HumidityDto) => {
+    return this.db.humidity.create(data);
+  };
+
+  readHumidity = (filter?: any): Promise<HumidityDto[]> => {
+    return this.db.humidity.read(filter);
+  };
+
+  updateHumidity= (
+    id: string,
+    update: HumidityDto
+  ): Promise<HumidityDto | never> => {
+    return this.db.humidity.updateById(id, update);
+  };
+
+  deleteHumidity = (id: string): Promise<boolean> => {
+    return this.db.humidity.deleteById(id);
+  };
+}
+
