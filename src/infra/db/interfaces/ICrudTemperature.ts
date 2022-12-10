f2a8c96c592b29aeb8c2f5d8a4179360a243c3dc
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2022-12-10 10:18:20 +0100

    feat: add readById on temperature Schema

--- src/infra/db/interfaces/ICrudTemperature.ts
+++ src/infra/db/interfaces/ICrudTemperature.ts
@@ -3,6 +3,7 @@ import { TemperatureDto } from "@temperature/dtos/temperatureDto";
 export default interface ICrudTemperature {
   create(data: TemperatureDto): Promise<boolean>;
   read(filter: any): Promise<TemperatureDto[]>;
+  readById(id: string): Promise<TemperatureDto | null>;
   updateById(id: string, data: TemperatureDto): Promise<TemperatureDto | never>;
   deleteById(id: string): Promise<boolean>;
 }
