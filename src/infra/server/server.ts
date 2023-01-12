02fbc4f4b7135c71c57d5031242aa9037dc2ab45
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2023-01-12 13:01:24 +0100

    refactor: change db to mongo(main db)

--- src/infra/server/server.ts
+++ src/infra/server/server.ts
@@ -7,7 +7,7 @@ import Postgres from '@db/implementation/postgres/postgres';
 const PORT = Environment.express.port || 3000;
 
 const server = async () => {
-  const app = await application(new Postgres());
+  const app = await application(new MongoDB());
   app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
   });
