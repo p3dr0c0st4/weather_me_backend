48af2159b4db003eec6d52d661071b168f36273b
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2023-01-29 15:10:02 +0100

    feat: changed databases interfaces

--- src/infra/db/implementation/mockDb/MockDb.ts
+++ src/infra/db/implementation/mockDb/MockDb.ts
@@ -3,14 +3,18 @@ import ICrudTemperature from '@db/interfaces/ICrudTemperature'
 import { IDatabase } from '@db/interfaces/IDatabase'
 import TemperatureRepository from './repositories/TemperatureRepository'
 import HumidityRepository from './repositories//HumidityRepository'
+import ICrudUser from '@db/interfaces/ICrudUser'
+import UserRepository from '../mongodb/repositories/UserRepository'
 
 export default class MockDb implements IDatabase {
     temperature: ICrudTemperature
     humidity: ICrudHumidity
+    user: ICrudUser
 
     constructor() {
         this.temperature = new TemperatureRepository()
         this.humidity = new HumidityRepository()
+        this.user = new UserRepository()
     }
     init(): void {
         return
--- src/infra/db/implementation/postgres/postgres.ts
+++ src/infra/db/implementation/postgres/postgres.ts
@@ -1,17 +1,21 @@
 import ICrudHumidity from '@db/interfaces/ICrudHumidity'
 import ICrudTemperature from '@db/interfaces/ICrudTemperature'
+import ICrudUser from '@db/interfaces/ICrudUser'
 import { IDatabase } from '@db/interfaces/IDatabase'
 import { TemperatureDto } from '@temperature/dtos/temperatureDto'
+import UserRepository from '../mongodb/repositories/UserRepository'
 import HumidityRepository from '../postgres/repositories/HumidityRepository'
 import TemperatureRepository from '../postgres/repositories/TemperatureRepository'
 
 export default class Postgres implements IDatabase {
     temperature: ICrudTemperature
     humidity: ICrudHumidity
+    user: ICrudUser
 
     constructor() {
         this.temperature = new TemperatureRepository()
         this.humidity = new HumidityRepository()
+        this.user = new UserRepository()
     }
 
     async init(): Promise<void> {
